c4b48852c429efdff7a5b53bfad58ca443213b1a
Hem sopheak <kobsari35@gmail.com>
2025-11-25 19:07:48 +0800

    Update home route message in keep_alive.py

--- keep_alive.py
+++ keep_alive.py
@@ -5,9 +5,10 @@ app = Flask('')
 
 @app.route('/')
 def home():
-    return "Bot is running!"
+    return "Hello. I am alive! Bot is running."
 
 def run():
+    # Render ត្រូវការ Port 8080
     app.run(host='0.0.0.0', port=8080)
 
 def keep_alive():
